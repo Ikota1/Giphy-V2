074aedec7957826f205221c2fc2266729020b9f0
Daniil <dnazarov870@gmail.com>
2023-11-19 20:51:40 +0200

    Logo.tsx: update image routing

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -4,7 +4,7 @@ import styles from './Logo.module.css';
 const Logo: React.FunctionComponent = () => {
   return (
     <div className={styles.logo_wrapper}>
-      <img src="src/assets/logo_blacktext.png" className={styles.logo} alt="logo" />
+      <img src="src/assets/images/logo_blacktext.png" className={styles.logo} alt="logo" />
     </div>
   )
 }
