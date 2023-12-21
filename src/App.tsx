8cbf2d3baa255a0c4d00309975f7f96e918b2b9c
Daniil <dnazarov870@gmail.com>
2023-12-21 14:36:02 +0200

    App.tsx & App.module.tsx: remove redundant code and styling update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import styles from './App.module.css';
 import Header from './components/Header/Header';
-import Content from './components/Content/Content';
+import styles from './App.module.css';
+import Sidebar from './components/MainContent/Sidebar';
+import { Home } from './pages/Home';
+import { AppRouter } from './components/AppRouter';
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <div className={styles.App}>
       <Header />
-      <Content />
+      <div className={styles.Content}>
+        <Sidebar />
+        {<Home /> && <AppRouter />}
+      </div>
     </div>
+
   )
 }
 
