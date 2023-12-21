f72a519ca76438bd45b50b651dd8f93dbfb41b3f
Daniil <dnazarov870@gmail.com>
2023-12-21 14:32:11 +0200

    add: Content.tsx file and styling

---
+++ src/components/MainContent/Content.tsx
@@ -0,0 +1,9 @@
+import styles from './Content.module.css';
+
+const Content = () => {
+    return (
+        <div className={styles.content}>Main Content salam</div>
+    )
+}
+
+export default Content;
