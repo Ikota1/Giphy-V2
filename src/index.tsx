4e05b8b424da6952db63e943901a6197598dd3e9
Daniil <dnazarov870@gmail.com>
2023-12-21 14:42:19 +0200

    add: store.tsx file and update index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,13 @@
 import App from './App.tsx'
+import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
+import { store } from './store/store.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+)
